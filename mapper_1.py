a880632de49b4b0627bfba0840749b59065df6ca
kikyo1012 <110764653+kikyo1012@users.noreply.github.com>
2024-02-28 18:50:31 -0500

    mapper_1.py

--- mapper_1.py
+++ mapper_1.py
@@ -8,9 +8,12 @@ Created on Tue Feb 27 17:10:23 2024
 import re
 import sys
 
-pattern = re.compile(r'(?P<ip>\d+\.\d+\.\d+\.\d+).*?\[(?:\d{2}/\w{3}/\d{4}):(?P<hour>\d{2}):\d{2}:\d{2} \+\d{4}\].*?')
+pattern = re.compile(r'(?P<ip>\d+\.\d+\.\d+\.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
 
 for line in sys.stdin:
+    line = line.strip()
     match = pattern.search(line)
     if match:
-        print('%s\t%s' % ('[' + match.group('hour') + ':00]' + match.group('ip'), 1))
+        print("[{hour}:00]{ip}\t1".format(hour=match.group('hour'), ip=match.group('ip')))
+
+
