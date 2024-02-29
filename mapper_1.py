f872a7a08d5d39c8f5aff8dd2bd6a8ce9d85d764
kikyo1012 <110764653+kikyo1012@users.noreply.github.com>
2024-02-29 16:40:31 -0500

    Add files via upload

---
+++ mapper_1.py
@@ -0,0 +1,11 @@
+
+import re
+import sys
+
+pattern = re.compile(r'(?P<ip>\d+\.\d+\.\d+\.\d+).*?\[(?:\d{2}/\w{3}/\d{4}):(?P<hour>\d{2}):\d{2}:\d{2} \+\d{4}\].*?')
+
+for line in sys.stdin:
+    line = line.strip()
+    match = pattern.search(line)
+    if match:
+        print('%s\t%s' % ('[' + match.group('hour') + ':00]' + match.group('ip'), 1))
