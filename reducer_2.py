e3a43e51ea80ee7754badcde7881b4c6892ad493
kikyo1012 <110764653+kikyo1012@users.noreply.github.com>
2024-02-29 20:07:31 -0500

    Add files via upload

---
+++ reducer_2.py
@@ -0,0 +1,20 @@
+#reducer
+import sys
+from operator import itemgetter
+from collections import defaultdict
+
+
+ip_counts = defaultdict(int)
+
+# aggregate
+for line in sys.stdin:
+    hour, ip, count = line.strip().split('\t')
+    ip_counts[ip] += int(count)
+
+# get top 3
+top_ips = sorted(ip_counts.items(), key=lambda x: x[1], reverse=True)[:3]
+
+# output
+for ip, count in top_ips:
+    print("{}\t{}".format(ip, count))
+
