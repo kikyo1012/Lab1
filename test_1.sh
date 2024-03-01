9290b965958d087ade8d980fcb4ea8e3dd420748
kikyo1012 <110764653+kikyo1012@users.noreply.github.com>
2024-02-29 21:21:16 -0500

    Update test_1.sh

--- test_1.sh
+++ test_1.sh
@@ -1,4 +1,4 @@
-/start.sh
+bash start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /Lab1/input/
@@ -15,4 +15,4 @@
 /usr/local/hadoop/bin/hdfs dfs -cat /Lab1/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/output/
-/stop.sh
+bash stop.sh
