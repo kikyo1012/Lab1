389b855cae9cc84b529a427957c5a091b004e4d8
kikyo1012 <110764653+kikyo1012@users.noreply.github.com>
2024-02-29 23:43:53 -0500

    Update test_1.sh

--- test_1.sh
+++ test_1.sh
@@ -1,30 +1,14 @@
 #!/bin/sh
-
-# Start Hadoop services
 ../../start.sh
-
-# Remove existing HDFS directories if they exist
-/usr/local/hadoop/bin/hdfs dfs -rm -r /lab1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /lab1/output/
-
-# Create new HDFS directory for input
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /lab1/input/
-
-# Copy log file from local filesystem to HDFS
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /lab1/input/
-
-# Run the Hadoop MapReduce job
+/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /logstat/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../../mapreduce-test-python/lab1/mapper_1.py    -mapper ../../mapreduce-test-python/lab1/mapper_1.py \
--file ../../mapreduce-test-python/lab1/reducer_1.py   -reducer ../../mapreduce-test-python/lab1/reducer_1.py \
--input /lab1/input/* -output /lab1/output/
-
-# Output the results to the console
-/usr/local/hadoop/bin/hdfs dfs -cat /lab1/output/part-00000
-
-# Clean up: remove the HDFS directories used for input and output
-/usr/local/hadoop/bin/hdfs dfs -rm -r /lab1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /lab1/output/
-
-# Stop Hadoop services
+-file ../../mapreduce-test-python/logstat/mapper.py -mapper ../../mapreduce-test-python/logstat/mapper.py \
+-file ../../mapreduce-test-python/logstat/reducer.py -reducer ../../mapreduce-test-python/logstat/reducer.py \
+-input /logstat/input/* -output /logstat/output/
+/usr/local/hadoop/bin/hdfs dfs -cat /logstat/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat/output/
 ../../stop.sh
