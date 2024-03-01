5a11af21334e6a0ff1b21789507c71b9c5402ae2
kikyo1012 <110764653+kikyo1012@users.noreply.github.com>
2024-02-29 21:24:19 -0500

    Update test_1.sh

--- test_1.sh
+++ test_1.sh
@@ -1,18 +1,18 @@
 bash start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /Lab1/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../Lab1/access.log /Lab1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /access.log /input/
 
 #read -p "Enter the hour range (e.g., 0-1): " hour_range
 
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
-#-file ../Lab1/mapper_1.py -mapper "python mapper_1.py $hour_range" \
--file ../Lab1/mapper_1.py -mapper ../Lab1/mapper_1.py \
+#-file /mapper_1.py -mapper "python mapper_1.py $hour_range" \
+-file /mapper_1.py -mapper /mapper_1.py \
 
--file ../Lab1/reducer_1.py -reducer ../Lab1/reducer_1.py \
--input /Lab1/input/* -output /Lab1/output/
-/usr/local/hadoop/bin/hdfs dfs -cat /Lab1/output/part-00000
-/usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/output/
+-file /reducer_1.py -reducer /reducer_1.py \
+-input /input/* -output /output/
+/usr/local/hadoop/bin/hdfs dfs -cat /output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /output/
 bash stop.sh
